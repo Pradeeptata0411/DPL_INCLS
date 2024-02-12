ba508fb0832f5cce788ac9aa913fc665a241fa57
Your Name <pradeeptata0411@gmail.com>
2024-02-12 12:06:25 +0530

    add new code for main to call the function

--- in class activity 7/Main_optimizer.py
+++ in class activity 7/Main_optimizer.py
@@ -1,14 +1,16 @@
 from Preprocessing_Optimizers import PreProcess_Data
-import Classification_Optimizer as cm
+import Model as cm
 
 if __name__ == "__main__":
-    images_folder_path = 'E:\\KLU\\3rd year\\3_2\\deep learning\\Deep Learning Programs\\imagefile'
+    images_folder_path = 'E:\\KLU\\3rd year\\3_2\\deep learning\\Deep Learning Programs\\train'
     imdata = PreProcess_Data()
     imdata.visualization_images(images_folder_path, 4)
     imagefile, label, df = imdata.preprocess(images_folder_path)
     tr_gen, tt_gen, va_gen = imdata.generate_train_test_images(imagefile, label)
-    image_shape=(28,28,3)
-    model_adam=cm.DeepANN.simple_model(image_shape,optimizer='adam')
-    model_sgd=cm.DeepANN.simple_model1(image_shape,optimizer='sgd')
-    model_rmsprop=cm.DeepANN.simple_model2(image_shape,optimizer='rmsprop')
-    cm.compare_model([model_adam,model_sgd,model_rmsprop] ,tr_gen,va_gen,epochs=3)
+    image_shape=(128,128,3)
+    model = cm.Rnn_model(image_shape)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(tr_gen, validation_data=va_gen, epochs=2)
+    loss, accuracy = model.evaluate(tt_gen)
+    print("Test Loss:", loss)
+    print("Test Accuracy:", accuracy)
